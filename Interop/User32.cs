1f8e21bee914b305b86a75509464213183cb1f33
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2017-10-19 19:20:28 -0400

    Initial Commit

---
+++ Interop/User32.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: cdahmedeh
+ * Date: 2017-10-19
+ * Time: 6:52 PM
+ * 
+ */
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace MuraleWinCommand
+{
+	public static class User32
+	{
+		[DllImport("user32.dll", SetLastError = true)]
+		public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+		
+		[DllImport("user32.dll", CharSet = CharSet.Auto)]
+	    public static extern int SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: cdahmedeh
+ * Date: 2017-10-19
+ * Time: 6:34 PM
+ * 
+ */
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using CommandLine;
+
+namespace MuraleWinCommand
+{
+	class Options
+	{
+		[Option('f', "file", Required = true, HelpText = "Full absolute path of wallpaper image file")]
+		public string File { get; set; }
+		
+		[ParserState]
+		public IParserState LastParserState { get; set; }
+	}
+	
+	class Program
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			Options options = new Options();
+			bool valid = CommandLine.Parser.Default.ParseArgumentsStrict(args, options);
+			string file = options.File;
+			
+			if (valid) {
+				SetWallpaper(file);
+			} else {
+				Console.WriteLine("Invalid Command Line Arguments Provided");
+			}
+		}
+		
+		private static void SetWallpaper(String file)
+		{
+			WALLPAPEROPT wallpaperOpt = new WALLPAPEROPT();
+			wallpaperOpt.dwStyle = WallPaperStyle.WPSTYLE_CROPTOFIT;
+			wallpaperOpt.SizeOf = Marshal.SizeOf(typeof(WALLPAPEROPT));
+			
+			IntPtr programWindow = User32.FindWindow("Progman", null);
+			User32.SendMessage(programWindow, 0x52c, IntPtr.Zero, IntPtr.Zero);
+			
+			IActiveDesktop activeDesktop = ActiveDesktop.Create();
+			activeDesktop.SetWallpaper(file, 0);
+			activeDesktop.SetWallpaperOptions(ref wallpaperOpt, 0);
+			activeDesktop.ApplyChanges(AD_Apply.ALL | AD_Apply.FORCE | AD_Apply.BUFFERED_REFRESH);
+		}
+	}
+}
